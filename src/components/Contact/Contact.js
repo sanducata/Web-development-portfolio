117d0e4da9cadf90f0e9b996eaf84bfd38aaac00
Catalin Sandu <cata.sandu11@gmail.com>
2022-07-27 16:40:18 +0300

    Created a contact form
    - Added the send email functionality with EmailJS

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,61 @@
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
+
 import classes from "./contact.module.css";
 
 function Contact() {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_eqb5wx2",
+        "template_d55bxbl",
+        form.current,
+        "FaLNIlAW2BVq_0xRA"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className={classes.contactContainer} id="Contact">
-      Contact
+      <div className={classes.contactLeft}>
+        <h1>Contact</h1>
+        <p>If you want to send me a message just fill out the form below.</p>
+        <p className={classes.lastP}>I am happy to hear from you!</p>
+      </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <h1 className={classes.formTitle}>
+          Let's get in touch!
+          <img
+            className={classes.paperPlaneImg}
+            src={PF + "Images/envelope.svg"}
+            alt="paperPlane"
+          />
+        </h1>
+
+        <input type="text" name="user_name" placeholder="Your name" required />
+        <input
+          type="email"
+          name="user_email"
+          placeholder="Your email"
+          required
+        />
+        <textarea name="message" placeholder="Message" />
+        <button type="submit" value="Send">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,26 +6,28 @@ import classes from "./home.module.css";
 function Home() {
   return (
     <div className={classes.homeContainer} id="Home">
-      <div className={classes.profileImageDiv}>
-        <img
-          className={classes.profileImage}
-          src="http://localhost:3000/Images/IMG_3318.JPG"
-          alt="profileImage"
-        />
+      <div className={classes.homeWrapper}>
+        <div className={classes.profileImageDiv}>
+          <img
+            className={classes.profileImage}
+            src="http://localhost:3000/Images/IMG_3318.JPG"
+            alt="profileImage"
+          />
+        </div>
+        <div className={classes.profileImageDiv1}></div>
+        <div className={classes.heroSectionDetails}>
+          <p className={classes.myNameIsP}>Hi, my name is</p>
+          <h1 className={classes.nameH1}>Catalin Sandu</h1>
+          <p className={classes.subtitleP}>
+            I'm a <span className={classes.colorSpan}>web developer</span> based
+            in Sibiu and I like to build unique user interfaces.
+          </p>
+          <Link to="Work" smooth={true} duration={800}>
+            <div className={classes.workButton}>Work</div>
+          </Link>
+        </div>
+        <ScrollDownArrow />
       </div>
-      <div className={classes.profileImageDiv1}></div>
-      <div className={classes.heroSectionDetails}>
-        <p className={classes.myNameIsP}>Hi, my name is</p>
-        <h1 className={classes.nameH1}>Catalin Sandu</h1>
-        <p className={classes.subtitleP}>
-          I'm a <span className={classes.colorSpan}>web developer</span> based
-          in Sibiu and I like to build unique user interfaces.
-        </p>
-        <Link to="Work" smooth={true} duration={800}>
-          <div className={classes.workButton}>Work</div>
-        </Link>
-      </div>
-      <ScrollDownArrow />
     </div>
   );
 }
