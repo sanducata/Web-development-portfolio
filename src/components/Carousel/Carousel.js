b2a9b57337b0e088c38141d75bbd72bc63f9fa3f
Catalin Sandu <cata.sandu11@gmail.com>
2022-07-28 14:08:54 +0300

    Updated the arrows on the carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -24,8 +24,10 @@ function Carousel() {
       swiperLeftArrowRef.current.classList.contains("swiper-button-disabled")
     ) {
       swiperLeftArrowRef.current.style.backgroundImage = `url(${LeftArrowDisabled})`;
+      swiperLeftArrowRef.current.style.border = "1px solid #b2b3ba";
     } else {
       swiperLeftArrowRef.current.style.backgroundImage = `url(${LeftArrow})`;
+      swiperLeftArrowRef.current.style.border = "1px solid #0a1128";
       if (isHoveringLeftArrow)
         swiperLeftArrowRef.current.style.transform = "translateX(-3px)";
     }
@@ -37,8 +39,10 @@ function Carousel() {
       swiperRightArrowRef.current.classList.contains("swiper-button-disabled")
     ) {
       swiperRightArrowRef.current.style.backgroundImage = `url(${RightArrowDisabled})`;
+      swiperRightArrowRef.current.style.border = "1px solid #b2b3ba";
     } else {
       swiperRightArrowRef.current.style.backgroundImage = `url(${RightArrow})`;
+      swiperRightArrowRef.current.style.border = "1px solid #0a1128";
       if (isHoveringRightArrow)
         swiperRightArrowRef.current.style.transform = "translateX(3px)";
     }
@@ -76,56 +80,58 @@ function Carousel() {
   };
 
   return (
-    <Swiper
-      modules={[Navigation]}
-      navigation={{
-        prevEl: swiperLeftArrowRef.current,
-        nextEl: swiperRightArrowRef.current,
-      }}
-      onInit={(swiper) => {
-        swiper.params.navigation.prevEl = swiperLeftArrowRef.current;
-        swiper.params.navigation.nextEl = swiperRightArrowRef.current;
-        swiper.navigation.init();
-        swiper.navigation.update();
-      }}
-      allowTouchMove={false}
-      speed={800}
-      spaceBetween={10}
-      slidesPerView={2}
-      className={classes.carouselSwiper}
-    >
-      {Projects.map((project) => {
-        return (
-          <SwiperSlide className={classes.swiperSlide} key={project.id}>
-            <Card
-              title={project.title}
-              description={project.description}
-              categories={project.categories}
-              links={project.links}
-              comingSoon={project.comingSoon}
-            />
-          </SwiperSlide>
-        );
-      })}
-      <div className={classes.leftArrowBehind}>
-        <div
-          className={classes.swiperLeftArrow}
-          ref={swiperLeftArrowRef}
-          onClick={leftArrowClick}
-          onMouseEnter={handleMouseEnterLeftArrow}
-          onMouseLeave={handleMouseLeaveLeftArrow}
-        ></div>
-      </div>
-      <div className={classes.rightArrowBehind}>
-        <div
-          className={classes.swiperRightArrow}
-          ref={swiperRightArrowRef}
-          onClick={rightArrowClick}
-          onMouseEnter={handleMouseEnterRightArrow}
-          onMouseLeave={handleMouseLeaveRightArrow}
-        ></div>
-      </div>
-    </Swiper>
+    <div className={classes.carouselWrapper}>
+      <Swiper
+        modules={[Navigation]}
+        navigation={{
+          prevEl: swiperLeftArrowRef.current,
+          nextEl: swiperRightArrowRef.current,
+        }}
+        onInit={(swiper) => {
+          swiper.params.navigation.prevEl = swiperLeftArrowRef.current;
+          swiper.params.navigation.nextEl = swiperRightArrowRef.current;
+          swiper.navigation.init();
+          swiper.navigation.update();
+        }}
+        allowTouchMove={false}
+        speed={800}
+        spaceBetween={10}
+        slidesPerView={2}
+        className={classes.carouselSwiper}
+      >
+        {Projects.map((project) => {
+          return (
+            <SwiperSlide className={classes.swiperSlide} key={project.id}>
+              <Card
+                title={project.title}
+                description={project.description}
+                categories={project.categories}
+                links={project.links}
+                comingSoon={project.comingSoon}
+              />
+            </SwiperSlide>
+          );
+        })}
+        <div className={classes.leftArrowBehind}>
+          <div
+            className={classes.swiperLeftArrow}
+            ref={swiperLeftArrowRef}
+            onClick={leftArrowClick}
+            onMouseEnter={handleMouseEnterLeftArrow}
+            onMouseLeave={handleMouseLeaveLeftArrow}
+          ></div>
+        </div>
+        <div className={classes.rightArrowBehind}>
+          <div
+            className={classes.swiperRightArrow}
+            ref={swiperRightArrowRef}
+            onClick={rightArrowClick}
+            onMouseEnter={handleMouseEnterRightArrow}
+            onMouseLeave={handleMouseLeaveRightArrow}
+          ></div>
+        </div>
+      </Swiper>
+    </div>
   );
 }
 
